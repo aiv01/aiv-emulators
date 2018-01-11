ce826bcb813c9422642b2a979ba13c101d56e317
Roberto De Ioris <roberto@20tab.com>
2018-01-11 11:41:06 +0100

    added NOP

--- cpu/mos6502/src/mos6502.c
+++ cpu/mos6502/src/mos6502.c
@@ -14,6 +14,7 @@ int mos6502_tick(mos6502_t *cpu) {
 void mos6502_init(mos6502_t *cpu){
     memset(cpu, 0, sizeof(mos6502_t));
     and_init(cpu);
+    nop_init(cpu);
 }
 
 unsigned char mos6502_ram_read8(mos6502_t *cpu, unsigned short address)
