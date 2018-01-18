018b1bb8d30d63f219dd0748130bbac684b500ff
Roberto De Ioris <roberto@20tab.com>
2018-01-18 07:52:59 +0100

    added ppu putpixel

--- machine/nes/ppu.c
+++ machine/nes/ppu.c
@@ -1,6 +1,13 @@
 #include <nes.h>
 
+static void ppu_putpixel(SDL_Renderer *renderer, int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+	SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+	SDL_RenderDrawPoint(renderer, x, y);
+}
+
 int ppu_tick(SDL_Renderer *renderer)
 {
+	ppu_putpixel(renderer, 100, 100, 255, 0, 0);
 	return 0;
 }
